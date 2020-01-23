53e8a2b19873f432b7869ddc503dfdd1d9833353
Jesse Li <jesse.li2002@gmail.com>
2020-01-23 10:51:18 -0800

    Q1 done'

---
+++ q1.py
@@ -0,0 +1,37 @@
+import numpy as np
+import scipy.stats
+import matplotlib.pyplot as plt
+
+
+# --------------------------------
+# PART 1 -------------------------
+# --------------------------------
+
+s = 1
+
+M_choices = [1, 4, 16, 32]
+large_N = 1000
+
+for M in M_choices:
+    vals = np.zeros(large_N)
+    for i in range(large_N):
+        delta = ((np.random.rand(M) > 0.5) * 2 - 1) * s / np.sqrt(M)
+        epsilon = sum(delta)
+        vals[i] = epsilon
+
+    smallest_possible = -M * s / np.sqrt(M)
+    largest_possible = M * s / np.sqrt(M)
+    bins = np.linspace(smallest_possible, largest_possible, M + 2)
+
+
+    ideal_x = np.linspace(smallest_possible * (1 + 1 / M), largest_possible * (1 + 1 / M), 200)
+    ideal_y = scipy.stats.norm.pdf(ideal_x, 0, 1)
+
+    plt.hist(vals, bins=bins, density=True)
+    plt.plot(ideal_x, ideal_y, color='orange')
+    plt.show()
+
+
+
+
+
